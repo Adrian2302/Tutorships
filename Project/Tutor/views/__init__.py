96ae20fb61314da76a06797479b901e6934c01c5
Alonso Moreno <jesus.moreno@ucr.ac.cr>
2021-10-30 00:02:47 -0600

    Manejo de solicitudes.

--- Project/Tutor/views/__init__.py
+++ Project/Tutor/views/__init__.py
@@ -1,9 +1,11 @@
 from .CalendarView import CalendarView
 from .ProfileView import ProfileView
 from .CourseView import CourseView
+from .PendingRequestView import PendingRequestView
 
 __all__ = [
     CalendarView,
     ProfileView,
     CourseView,
+    PendingRequestView
 ]
