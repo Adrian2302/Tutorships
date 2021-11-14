fa200ebd89fe1a2029a5170598169300660eb6e9
Fonsii <lualfonso14200174@gmail.com>
2021-11-14 17:20:52 -0600

    refactor student search and display. Adding the function to a guest in a tutor private group

--- Project/Student/views/requestTutorship.py
+++ Project/Student/views/requestTutorship.py
@@ -1,19 +1,52 @@
-import datetime
+from datetime import datetime, timedelta
 from django.views import generic
 from django.shortcuts import redirect, render
+from django.core.cache import cache
 
-from Student.models import Request
+from Student.models import Request, Requesters
 from UserAuthentication.models import User
 from Session.models import Session
 from Modality.models import Modality
 from Course.models import Course
+from Tutor.models import TutorAvailableSchedule, Tutor
+from Payment.models import Payment
 
-def check_values_get(request):
-    if request.GET.get('tutor') is not None and request.GET.get('fecha') and request.GET.get(
-            'sesion') is not None and request.GET.get('modalidad') is not None and request.GET.get(
-        'inicial') is not None and request.GET.get('final') is not None:
-        return True
-    return False
+instance_id = None
+
+def save_values_post(request):
+    dict_values = {}
+
+    dict_values['user_requester'] = request.user
+
+    if request.POST.get('fecha'):
+        dict_values['fecha'] = request.POST.get('fecha')
+
+    if request.POST.get('tutor'):
+        dict_values['tutor'] = request.POST.get('tutor')
+    
+    if request.POST.get('sesion'):
+        dict_values['sesion'] = request.POST.get('sesion')
+
+    if request.POST.get('modalidad'):
+        dict_values['modalidad'] = request.POST.get('modalidad')
+
+    if request.POST.get('inicial'):
+        dict_values['inicial'] = request.POST.get('inicial')
+
+    if request.POST.get('final'):
+        dict_values['final'] = get_added_hours(request.POST.get('final'))
+
+    if request.POST.getlist('invitados'):
+        dict_values['invitados'] = request.POST.getlist('invitados')
+
+    if valid_comment(request.POST.get('comentario')):
+        dict_values['comentario'] = request.POST.get('comentario')
+
+    return dict_values
+
+def check_dict_value(dict_values):
+    valid_keys = ('tutor', 'sesion', 'modalidad', 'inicial', 'final', 'fecha')
+    return all(key in dict_values for key in valid_keys)
 
 def valid_comment(comment: str):
     if comment is not None and comment.replace(" ", "") != "":
@@ -27,48 +60,132 @@ def get_added_hours(added_hours):
         return 2, 0
     return 1, 0
 
-def request_maker(request, course_name):
-    try:
-        request_builder = Request()
+def set_all_guests_request(dict_values, request_tutorship):
+    for guest in dict_values['invitados']:
+        print(guest)
+        resquester = Requesters()
+        resquester.request = request_tutorship
+        resquester.user_requester = User.objects.get(id=guest)
+        resquester.save()
 
-        request_builder.user_requester = User.objects.get(id=request.user.id)
-        request_builder.tutor_requested = User.objects.get(email=request.GET.get('tutor'))
-        request_builder.num_requesters = 1
-        request_builder.tutor_comment = None
-
-        if valid_comment(request.GET.get('comentario')):
-            request_builder.student_comment = request.GET.get('comentario')
+def check_guests(dict_values):
+    if dict_values['sesion'] == "Grupal - Privada":
+        if 'invitados' in dict_values:
+            return True
         else:
-            request_builder.student_comment = None
+            raise Exception("No se han seleccionado invitados en una Grupal - Privada")
+    return False
 
-        request_builder.session_requested = Session.objects.get(name=request.GET.get('sesion'))
-        request_builder.modality_requested = Modality.objects.get(name=request.GET.get('modalidad'))
-        request_builder.course_requested = Course.objects.get(course_name=course_name)
+def request_maker(dict_values, course_name):
+    try:
+        schedule_selected = cache.get('schedule_selected')
 
-        start_date = datetime.datetime.strptime(request.GET.get('fecha'), "%d/%m/%Y")
-        start_date_end_values = start_date.replace(hour=int(request.GET.get('inicial').split(":")[0]),
-                                                minute=int(request.GET.get('inicial').split(":")[1]))
-        hour, minutes = get_added_hours(request.GET.get('final'))
-        hours_added = datetime.timedelta(hours=hour, minutes=minutes)
+        user_requester = User.objects.get(id=dict_values['user_requester'].id)
+        tutor_requested = User.objects.get(id=dict_values['tutor'])
 
-        request_builder.date_start = start_date_end_values
-        request_builder.date_end = start_date_end_values + hours_added
-        request_builder.date_resolution = start_date_end_values
+        num_requesters = 1
+        
+        session_requested = Session.objects.get(name=dict_values['sesion'])
+        modality_requested = Modality.objects.get(name=dict_values['modalidad'])
+        course_requested = Course.objects.get(course_name=course_name)
+
+        start_date = datetime.strptime(dict_values['fecha'], "%Y-%m-%d")
+        initial_hour, initial_minutes = dict_values['inicial'].split(":")
+
+        start_date = start_date.replace(hour=int(initial_hour), minute=int(initial_minutes))
+
+        final_hour, final_minutes = dict_values['final']
+        time_added = timedelta(hours=final_hour, minutes=final_minutes)
+
+        end_date = start_date + time_added
+        
+        do_requesters = check_guests(dict_values)
+            
+        request_builder = Request.objects.create(user_requester=user_requester,
+                                                 tutor_requested=tutor_requested,
+                                                 num_requesters=num_requesters,
+                                                 session_requested=session_requested,
+                                                 modality_requested=modality_requested,
+                                                 course_requested=course_requested,
+                                                 date_start=start_date,
+                                                 date_end=end_date,
+                                                 date_request=start_date)
+
+        if 'comentario' in dict_values:
+           request_builder.student_comment = dict_values['comentario']
 
         request_builder.save()
 
-        return True
-    except:
-        return False
+        print(request_builder.id)
+        if do_requesters:
+            set_all_guests_request(dict_values, request_builder)
+        
+    except Exception as e:
+        print(e)
+        raise Exception("Unknown exception")
+
+def create_context(schedule_id, user):
+    try:
+        context = {}
+        schedule_selected = TutorAvailableSchedule.objects.get(id=schedule_id)
+
+        cache.set('schedule_selected', schedule_selected)
+        tutor = Tutor.objects.get(user = schedule_selected.user)
+        all_students = User.objects.filter(type=1).exclude(id=user.id).order_by('name')
+
+        #Para la integración de varias sesiones se debe tener una lista de sesiones id y hacer id__in
+        sessions = Session.objects.filter(id=tutor.session_type.id)
+        modals = Modality.objects.filter(id=tutor.modality_type.id)
+        payments = Payment.objects.filter(id=tutor.payment_type.id)
+       
+        min_time = schedule_selected.start_time.strftime('%H:%M')
+        max_time = schedule_selected.end_time - timedelta(hours=1, minutes=0)
+        max_time = max_time.strftime('%H:%M')
+
+        context.update({
+            'tutor': tutor,
+            'students': all_students,
+            'sessions': sessions,
+            'modals': modals,
+            'payments': payments,
+            'min_time': min_time,
+            'max_time': max_time,
+            'value_tutorship_person': tutor.amount_per_person,
+            'increment': tutor.increment_per_half_hour,
+            'date':str(schedule_selected.start_time.date()),
+            'students':all_students
+        })
+
+        return context  
+    except schedule_selected.DoesNotExist:
+        raise Exception("Schedule selected id is not in the database")
+    except Exception as e:
+        print(e)
+        raise Exception("Unknown exception")
 
 class requestTutorship(generic.View):
 
     def get(self, request, course_name):
-        if request.user.is_authenticated:
-            if check_values_get(request):
-                success = request_maker(request, course_name)
-                return render(request, "Student/reportRequest.html", {'success': success})
+        user= User.objects.get(id=request.user.id)
+        if request.user.is_authenticated and user.is_student():
+            try:            
+                schedule_id = request.GET.get('schedule_number')
+                context = create_context(schedule_id, user)
+                return render(request, "Student/formRequestTutorship.html", context)
+            except Exception as e:
+                print(e)
+        return redirect('index')
 
-            return render(request, "Student/reportRequest.html", {'success': False})
-        else:
-            return redirect('index')
+    def post(self, request, course_name):
+        user= User.objects.get(id=request.user.id)
+        if request.user.is_authenticated and user.is_student():
+            try:
+                dict_values = save_values_post(request)
+
+                if check_dict_value(dict_values):          
+                    request_maker(dict_values, course_name)
+                    return render(request, "Student/reportRequest.html", {'success': True})
+                return render(request, "Student/reportRequest.html", {'success': False})
+            except:
+                return render(request, "Student/reportRequest.html", {'success': False})
+        return redirect('index')
--- Project/Student/views/searchCourse.py
+++ Project/Student/views/searchCourse.py
@@ -7,11 +7,14 @@ from django.conf import settings
 from Course.models import Course
 from Session.models import Session
 from Modality.models import Modality
+from UserAuthentication.models import TutorCourse, User
 
 def create_context(search_query, page_number):	
+
     results = get_results(search_query)
-    paginator = Paginator(results, settings.PAGE_SIZE)
-    page_display = paginator.get_page(page_number)
+
+    page_display = get_paginator_results(results, page_number)
+
     sessions = Session.objects.all()
     modals = Modality.objects.all()
 
@@ -24,19 +27,61 @@ def create_context(search_query, page_number):
 
     return context
 
+def get_paginator_results(results, page_number):
+    if results is not None:
+        if page_number is None:
+            page_number = 1
+
+        paginator = Paginator(results, settings.PAGE_SIZE)
+        page_display = paginator.get_page(page_number)
+        return page_display
+    return results
+
 def get_results(search_query):
     results = None
     if cache.get('latest_search') == search_query and cache.get('latest_results') is not None:
         results = cache.get('latest_results')
     else:
         if search_query == "all" or search_query == "":
-            results = Course.objects.all()
+            results = Course.objects.all().order_by('course_name')
         else:
-            results = Course.objects.filter(course_name__icontains=search_query)
+            results = Course.objects.filter(course_name__icontains=search_query).order_by('course_name')
         cache.set('latest_search', search_query)
     cache.set('latest_results', results)
 
     return results
+
+def get_results_by_filters(search_query, filters):
+    results = None
+
+def filter_tutor(last_query, tutor_fullname):
+    split_name = tutor_fullname.split(' ')
+    tutor = User.objects.filter(name=split_name[0], lastname=split_name[1])
+
+    if tutor is not None and tutor.is_tutor():
+        query_tutor_course = TutorCourse.objects.filter(user=tutor)
+        query_result = Course.objects.filter(tutor=tutor)
+    pass
+
+def filter_session(last_query, session_names):
+    pass
+
+def filter_modality(last_query, modality_names):
+    pass
+
+def get_filters(request):
+    filters = {}
+
+    if request.GET.get('filtroTutor'):
+        filters.update({'tutor': request.GET.get('filtroTutor')})
+
+    if request.GET.getlist('filtroModalidad'):
+        filters.update({'modality': request.GET.getlist('filtroModalidad')})
+    
+    if request.GET.getlist('filtroSesion'):
+        filters.update({'session': request.GET.getlist('filtroSesion')})
+        
+    return filters
 
 class searchCourse(generic.View):
 
