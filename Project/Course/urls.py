e4b867ed07976884ff8ccd23afa3b82307b5298d
Adrian2302 <barbambu123@gmail.com>
2021-11-14 15:20:35 -0600

    Admin edits

--- Project/Course/urls.py
+++ Project/Course/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("add_course", views.add_course, name="add_course")
+    path("add_course", views.AddCourse.as_view(), name="add_course"),
+    path('edit_course', views.EditCourse.as_view(), name='edit_course'),
+    path(r'edit_course/<int:course>', views.EditCourse.as_view(), name='edit_course'),
 ]
