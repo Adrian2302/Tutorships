e43cc6d437b88370644f2b1811a4ebc839ff6e3c
Fonsii <lualfonso14200174@gmail.com>
2021-11-17 14:16:29 -0600

    Filter sessions, modalities and payments. Added favico.ico

--- Project/Student/views/searchCourse.py
+++ Project/Student/views/searchCourse.py
@@ -8,7 +8,7 @@ from Session.models import Session
 from Modality.models import Modality
 from Payment.models import Payment
 from UserAuthentication.models import User
-from Tutor.models import TutorCourse
+from Tutor.models import Tutor, TutorCourse
 from Student.filtersModels import ListTypeSearch
 
 def create_context(search_query, page_number, type_search, filters):
@@ -24,6 +24,10 @@ def create_context(search_query, page_number, type_search, filters):
 
         if type_search == None:
             type_search = "cursos"
+
+        diplay_sessions_link = 0
+        if type_search == "tutor":
+            diplay_sessions_link = 1
         
         context = {
                 'latest_search': search_query,
@@ -32,7 +36,8 @@ def create_context(search_query, page_number, type_search, filters):
                 'sessions': sessions,   
                 'modals': modals,
                 'retributions' : retributions, 
-                'last_type': type_search
+                'last_type': type_search, 
+                'diplay_sessions_link': diplay_sessions_link
             }
     except Exception as e:
         print(e)
@@ -73,36 +78,41 @@ def get_results_university(search_query):
 
 
 def get_results_tutor(search_query, filters):
-    if search_query == "all" or search_query == "":
-        results = User.objects.filter(type=2).order_by('name', 'lastname')
-    else:
-        fullname = search_query.split(' ')
-        if len(fullname) == 2:
-            results = User.objects.filter(name__icontains=fullname[0], lastname__icontains=fullname[1], type=2).order_by('name', 'lastname')
+    try:
+        if search_query == "all" or search_query == "":
+            results = User.objects.filter(type=2).order_by('name', 'lastname')
         else:
-            results = User.objects.filter(name__icontains=fullname[0], type=2).order_by('name', 'lastname')
+            fullname = search_query.split(' ')
+            if len(fullname) == 2:
+                results = User.objects.filter(name__icontains=fullname[0], lastname__icontains=fullname[1], type=2).order_by('name', 'lastname')
+            else:
+                results = User.objects.filter(name__icontains=fullname[0], type=2).order_by('name', 'lastname')
 
-    results = do_filters(filters)
-    return results
+        results = do_filters(results, filters)
+        return results
+    except:
+        raise ValueError("Error in getting the results")
     
 
 def handlers_results(search_query, type_search, filters):
-
-    if type_search == "universidad":
-        type_list = get_list_type_search(1)
-        results = get_results_university(search_query)
-    elif type_search == "tutor":
-        type_list = get_list_type_search(2)
-        results = get_results_tutor(search_query, filters)
-    elif type_search == "recursos-publicos":
-        pass
-    elif type_search == "sesiones-abiertas":
-        pass
-    else:
-        type_list = get_list_type_search(0)
-        results = get_results_course(search_query)
-        
-    return results, type_list
+    try:
+        if type_search == "universidad":
+            type_list = get_list_type_search(1)
+            results = get_results_university(search_query)
+        elif type_search == "tutor":
+            type_list = get_list_type_search(2)
+            results = get_results_tutor(search_query, filters)
+        elif type_search == "recursos-publicos":
+            pass
+        elif type_search == "sesiones-abiertas":
+            pass
+        else:
+            type_list = get_list_type_search(0)
+            results = get_results_course(search_query)
+            
+        return results, type_list
+    except:
+        raise ValueError("Error in getting the results")
 
 
 def get_list_type_search(selected_type_search):
@@ -111,32 +121,72 @@ def get_list_type_search(selected_type_search):
     return list_type_search.list
 
 
-def do_filters(filters):
-    pass
+def do_filters(results, filters):
+    try:
+        filtered_results = results
+        if 'sessions' in filters:        
+            filtered_results = filter_session(filtered_results, filters['sessions'])
+
+        if 'modals' in filters:
+            filtered_results = filter_modality(filtered_results, filters['modals'])
+
+        if 'payments' in filters:
+            print("jbjbjkb")
+            filtered_results = filter_payment(filtered_results, filters['payments'])
+        
+        return filtered_results
+
+    except:
+        raise ValueError("Error in filtering the results")
 
 
 def filter_session(last_query, session_names):
-    pass
+    try:
+        sessions = Session.objects.filter(name__in=session_names).values_list('id', flat=True)
+        all_tutors = Tutor.objects.filter(session_type__in=sessions).values_list('user', flat=True)
+        filtered_results = last_query.filter(id__in=all_tutors)
+        
+        return filtered_results
+    except:
+        raise ValueError("Error in filtering the sesssion")
 
 
 def filter_modality(last_query, modality_names):
-    pass
+    try:
+        modalities = Modality.objects.filter(name__in=modality_names).values_list('id', flat=True)
+        all_tutors = Tutor.objects.filter(modality_type__in=modalities).values_list('user', flat=True)
+        filtered_results = last_query.filter(id__in=all_tutors)
+
+        return filtered_results
+    except:
+        raise ValueError("Error in filtering the modality")
+
+
+def filter_payment(last_query, payment_names):
+    try:
+        payments = Payment.objects.filter(name__in=payment_names).values_list('id', flat=True)
+        all_tutors = Tutor.objects.filter(payment_type__in=payments).values_list('user', flat=True)
+        filtered_results = last_query.filter(id__in=all_tutors)
+        
+        return filtered_results
+    except:
+        raise ValueError("Error in filtering the payment")
 
 
 def get_filters(request):
     filters = {}
 
-    if request.GET.get('sesion'):
-        filters['session'] = request.GET.get('sesion')
+    if request.GET.getlist('sesion'):
+        filters['sessions'] = request.GET.getlist('sesion')
     
     if request.GET.getlist('modalidad'):
-        filters['modality'] = request.GET.get('modalidad')
+        filters['modals'] = request.GET.getlist('modalidad')
 
-    if request.GET.get('retribución'):
-        filters['payment'] = request.GET.get('retribución')
+    if request.GET.getlist('retribucion'):
+        filters['payments'] = request.GET.getlist('retribucion')
 
-    if request.GET.get('calificacion'):
-        filters['score'] = request.GET.get('calificacion')
+    if request.GET.getlist('calificacion'):
+        filters['score'] = request.GET.getlist('calificacion')
 
     if request.GET.get('region'):
         pass
