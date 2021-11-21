ed118bbce369118b556e4d401e482b6a46b8fe48
Victor Mora Abarca <vmora206@gmail.com>
2021-11-21 17:06:22 -0600

    fix calendar bug v1.0

--- Project/Tutor/views/PendingRequestView.py
+++ Project/Tutor/views/PendingRequestView.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView
 from django import forms
 from UserAuthentication.models import User
 from django.shortcuts import render, redirect
-from Tutor.models import Tutor
+from Tutor.models import Tutor, TutorAvailableSchedule
 from Session.models import Session
 from Modality.models import Modality
 from Payment.models import Payment
@@ -52,6 +52,65 @@ class PendingRequestView(generic.View):
                     request=request_tutorship
                 )
                 tutorship.save()
+
+                start_date=request_tutorship.date_start
+                tutorship_start_date=start_date.strftime("%Y-%m-%d %H:%M")
+
+                end_date=request_tutorship.date_end
+                tutorship_end_date=end_date.strftime("%Y-%m-%d %H:%M")
+
+                # rejected other tutorships
+                query_set = list(Request.objects.filter(tutor_requested_id=user,
+                 state='PN',
+                  date_start__gte=tutorship_start_date))
+                for requests in query_set:
+                    date_requests = requests.date_start
+                    clean_date_requests = date_requests.strftime("%Y-%m-%d %H:%M")
+                    if tutorship_start_date == clean_date_requests or tutorship_end_date > clean_date_requests:
+                        requests.state = 'DD'
+                        requests.save()
+
+                        # Create notification
+                        notification = RequestNotification(
+                            notification_type='RR',
+                            to_user=requests.user_requester,
+                            from_user=requests.tutor_requested,
+                            request=requests
+                        )
+                        notification.save()
+                
+                # change tutor calendar
+                schedule_selected=list(TutorAvailableSchedule.objects.filter(user=user,
+                 start_time__lte=tutorship_start_date,
+                  end_time__gte=tutorship_end_date))
+
+                for schedule in schedule_selected:
+                    date_start=schedule.start_time
+                    schedule_date_start=date_start.strftime("%Y-%m-%d %H:%M")
+                    date_end=schedule.end_time
+                    schedule_date_end=date_end.strftime("%Y-%m-%d %H:%M")
+
+                    if schedule_date_start == tutorship_start_date and schedule_date_end == tutorship_end_date:
+                        schedule.delete()
+                    elif schedule_date_start == tutorship_start_date:
+                        schedule.start_time = tutorship_end_date
+                        schedule.save()
+                        print("HORA INICIO AL INICIO DEL HORARIO")
+                    elif schedule_date_end == tutorship_end_date:
+                        schedule.end_time = tutorship_start_date
+                        schedule.save()
+                        print("HORA FINAL AL FINAL DEL HORARIO")
+                    else:
+                        new_end_time = schedule_date_end
+                        schedule.end_time = tutorship_start_date
+                        scheduled_block = TutorAvailableSchedule(
+                            user_id=user,
+                            start_time=tutorship_end_date,
+                            end_time=new_end_time,
+                        )
+                        schedule.save()
+                        scheduled_block.save()
+                        print("AGENDÓ EN EL CENTRO DEL HORARIO")
 
                 # Create notification
                 notification = RequestNotification(
