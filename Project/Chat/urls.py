f9585340f62895354cb1df08ea54a4bb5b2b8f0c
Alonso Moreno <jesus.moreno@ucr.ac.cr>
2021-12-05 15:47:28 -0600

    send message

--- Project/Chat/urls.py
+++ Project/Chat/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('mensajes/', views.ChatLobby.as_view(), name='chatlobby'),
+    path('mensajes/enviar', views.SendMessage.as_view(), name='send_message'),
+
     path('mensajes/<int:user_receiver_pk>', views.ChatRoom.as_view(), name='chatroom'),
     path('mensajes/<int:room_pk>/<int:user_receiver_pk>', views.ChatRoom.as_view(), name='chatroom'),
 ]
