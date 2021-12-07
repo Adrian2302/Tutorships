6f3b93890bac46c545ee6b766b14c7619d495163
Alonso Moreno <jesus.moreno@ucr.ac.cr>
2021-12-06 21:02:28 -0600

    send message real time

--- Project/Chat/views/ChatLobby.py
+++ Project/Chat/views/ChatLobby.py
@@ -13,20 +13,26 @@ class ChatLobby(View):
     template_name = 'chatLobby.html'
     user: User = None
 
-    def get(self, request):
+    def get(self, request, room_pk=None):
         user = User.objects.get(pk=request.user.id)
         rooms = list(Room.objects.filter(original_user_sender=user) | Room.objects.filter(original_user_receiver=user))
 
+        current_room = None
+        messages = None
+        if room_pk:
+            current_room = Room.objects.get(pk=room_pk)
+            messages = list(Message.objects.filter(room=current_room))
         context = {
             'current_user': user,
             'rooms': rooms,
+            'current_room': current_room,
+            'messages': messages
         }
         return render(request, self.template_name, context)
 
     def post(self, request):
         if request.is_ajax():
             user = User.objects.get(pk=request.user.id)
-            # print(request.POST['room_id'])
 
             if request.POST.get('room_id'):
                 room = Room.objects.get(pk=request.POST['room_id'])
