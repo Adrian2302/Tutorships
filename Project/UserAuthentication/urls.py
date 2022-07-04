18d65b0cb8790967a510c9275917a853791dee0e
Fonsii <lualfonso14200174@gmail.com>
2022-07-04 10:23:23 -0600

    Adding photo profile to student and tutor in profile

--- Project/UserAuthentication/urls.py
+++ Project/UserAuthentication/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -8,4 +10,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view(), name="logout"),
     path('add_administrator', views.add_administrator, name="add_administrator"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
